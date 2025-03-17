6b2e2391e9e7a9b0c8b20dbcb69b65fdd2916879
hussain-wac <hussain.n@webandcrafts.in>
2025-03-17 17:23:33 +0530

    navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import MyCalendar from './MyCalendar'
 import RoomSelect from './Home/RoomSelect'
+import Navbar from './Layouts/Navbar'
 
 function Home() {
   return (
     <div>
+        <Navbar/>
+        
         <RoomSelect/>
     </div>
   )
